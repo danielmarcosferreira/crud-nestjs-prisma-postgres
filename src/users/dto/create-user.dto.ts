b6c647e4d3c7d598a02d1da8b4f97948ab746f46
danielmarcosferreira <daniel.marcos.1038@gmail.com>
2025-11-27 15:14:54 -0300

    feat: added Swagger to see the api documentation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,14 +1,28 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
 
 export class CreateUserDto {
+  @ApiProperty({
+    description: 'Nome do Usuário',
+    example: 'Jose da Silva',
+  })
   @IsNotEmpty()
   @IsString()
   name: string
 
+  @ApiProperty({
+    description: 'E-mail do usuário',
+    example: 'email@email.com',
+  })
   @IsNotEmpty()
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    description: 'Senha do usuário, com no mínimo 6 caracteres',
+    example: 'senhaSegura123',
+    minLength: 6
+  })
   @IsNotEmpty()
   @IsString()
   @MinLength(6)
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,11 +10,16 @@ import {
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ApiBody } from '@nestjs/swagger';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
-
+  
+  @ApiBody({
+    type: CreateUserDto,
+    description: 'Objeto para criacao de um novo usuario'
+  })
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
